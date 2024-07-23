397ecf835532ad000a7c59d9ad99bc884e5b3c11
Harikumarv98 <126553998+Harikumarv98@users.noreply.github.com>
2024-07-23 15:43:20 +0530

    Update WelcomeRestController.java

--- src/main/java/in/ashokit/rest/WelcomeRestController.java
+++ src/main/java/in/ashokit/rest/WelcomeRestController.java
@@ -18,7 +18,7 @@ public class WelcomeRestController {
 	@GetMapping
 	public String welcomeMsg() {
 		logger.info("***** welcomeMsg() execution start *****");
-		String msg = "Keerthi Raj AchiversIT ";
+		String msg = " AchiversIT And PariTech ";
 		logger.info("***** welcomeMsg() execution end *****");
 		return msg;
 	}
